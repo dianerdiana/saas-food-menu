3743de4c1cf4b0b6969a4f26890d9778b1d3cde4
dianerdiana <dianerdiana.de@gmail.com>
2025-12-15 21:40:40 +0700

    Delete: all

--- server/src/app/server.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-export async function createServer() {
-  const app = await NestFactory.create(AppModule);
-
-  return app;
-}
--- server/src/modules/user/application/use-cases/get-user.use-case.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class GetUserUseCase {
-  execute(): string {
-    return 'Hello world!';
-  }
-}
