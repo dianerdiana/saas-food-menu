88e8e5950f180202772532bcb3a09cb7ade7d9b1
dianerdiana <dianerdiana.de@gmail.com>
2025-12-17 21:14:17 +0700

    Update: Success page

--- apps/web/src/views/product/components/react/product-carousel.tsx
+++ apps/web/src/views/product/components/react/product-carousel.tsx
@@ -24,7 +24,7 @@ export function ProductCarousel({ products }: ProductCarouselProps) {
           {products.map((product, index) => (
             <CarouselItem key={index} className="w-fit basis-1/2 lg:basis-2/5">
               <div className=" p-3 pb-5 bg-white hover:bg-primary-slate rounded-xl border border-background hover:border-yellow-500 transition-all duration-300">
-                <a href="details.html">
+                <a href="/details">
                   <div className="flex flex-col w-full shrink-0 gap-2.5">
                     <div className="relative flex w-full h-37.5 shrink-0 rounded-xl bg-accent overflow-hidden">
                       <img
