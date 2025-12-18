00dc481c0090ecb9f836bcc193d7530a7df56ec8
dianerdiana <dianerdiana.de@gmail.com>
2025-12-18 09:51:22 +0700

    Update: add more ui components in packages/ui

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@workspace/ui/*": path.resolve(__dirname, "../../packages/ui/src/*"),
+    },
+  },
+  server: {
+    fs: {
+      // Mengizinkan Vite untuk melayani file di luar folder apps/web
+      // Ini penting agar komponen dari packages/ui bisa terbaca saat dev
+      allow: [".."],
+    },
+    watch: {
+      // Pastikan Vite memantau perubahan di folder packages/ui
+      ignored: ["!**/node_modules/@workspace/ui/**"],
+    },
+  },
+});
