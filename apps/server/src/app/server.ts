ff2feada87c73c3695a752e78622826eda8da246
dianerdiana <dianerdiana.de@gmail.com>
2025-12-21 18:55:31 +0700

    Add: validation pipe

--- apps/server/src/app/server.ts
+++ apps/server/src/app/server.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 export async function createServer() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   return app;
 }
