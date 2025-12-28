3465572f6d98bb1068a7c7b7275f8532735b7edc
dianerdiana <dianerdiana.de@gmail.com>
2025-12-28 10:15:33 +0700

    Update: fullName property to sign-in model

--- apps/server/src/modules/auth/domain/models/sign-in.model.ts
+++ apps/server/src/modules/auth/domain/models/sign-in.model.ts
@@ -1,9 +1,13 @@
-import { Exclude, Expose } from 'class-transformer';
+import { Exclude, Expose, Transform } from 'class-transformer';
 
 @Exclude()
 export class SignInModel {
   @Expose()
   id!: string;
+
+  @Expose()
+  @Transform(({ obj }) => `${obj.firstName}${obj.lastName ? ' ' + obj.lastName : ''}`)
+  fullName!: string;
 
   @Expose()
   email!: string;
