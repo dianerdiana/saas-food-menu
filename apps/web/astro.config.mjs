49775623d9686d465512e72acac0482493c0ef57
dianerdiana <dianerdiana.de@gmail.com>
2026-01-24 22:50:07 +0700

    Add docker to astro

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react()],
-  adapter: vercel(),
   devToolbar: {
     enabled: false,
   },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
