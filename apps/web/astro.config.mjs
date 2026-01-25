4a9fc91eee05076c91f59046b6e44917c7105400
dianerdiana <dianerdiana.de@gmail.com>
2026-01-25 15:23:33 +0700

    Move astro deployment place from vercel to vm

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,18 +2,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  trailingSlash: "always",
   integrations: [react()],
   devToolbar: {
     enabled: false,
   },
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
